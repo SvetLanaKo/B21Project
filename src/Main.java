37a598c2eab82a1976fdd4367ef82a515c12fec7
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-01-31 02:15:57 -0600

    new comment was added to the class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         // batch 21 is the best one
+        //everyone is happy with practicing
     }
 }
